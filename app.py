bcbc4f1c5d20a695e33c61c1678527271cecc9ad
mkulikovskiy <mkulikovskiy@d3led.com>
2020-03-10 17:25:17 -0700

    sending update POST

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import cgi, requests
+import cgi, requests, os
 from pprint import pprint
 
 from flask import Flask, render_template, request, jsonify
 from flask.json import dumps
 
 app = Flask('PitchUpdater')
+app.config.from_pyfile('config.cfg')
 
 @app.route('/')
 def hello():
@@ -15,59 +16,69 @@ def submit():
     leftPitch  = request.form['leftpitch']
     rightPitch = request.form['rightpitch']
 
-    pitches = ('1.6', '2.0', '2.5', '4.0')
-
+    pitches = app.config["PITCHES"]
     if (leftPitch not in pitches) or (rightPitch not in pitches):
         return "invalid request parameter"
 
     modules = get_modules()
     #return modules
-    if leftPitch == str(modules['left']['pitch']):
-        #return 'left pitch matched'
+
+    if leftPitch != str(modules['left']['pitch']):
+        #return 'left pitch requires update'
         return update_firmware(modules["left"])
-    if rightPitch == str(modules['right']['pitch']):
-        return 'right pitch matched'
-        #return update_firmware(modules['right']['mac'])
+    if rightPitch != str(modules['right']['pitch']):
+        #return 'right pitch requires update'
+        return update_firmware(modules['right'])
     return 'program finished'
 
 
     #return
 
-
+@app.route('/firm')
 def update_firmware(module):
     #return "in firm ware update"
     #return module
-    #TODO read token from config file not committed to GIT
-    ip  = "10.0.0.3"
+
+    ip = app.config["IP_ADDRESS"]
+    auth_token = app.config["AUTH_TOKEN"]
     url = 'http://' + ip + '/api/devices?command=update-firmware'
 
-    payload = {
-        "filename": firmwareFile,
-        "macs": mac,
-    }
-    headers = {
-        'Content-Type': "application/json",
-        #'Authorization': "Bearer " + token
-    }
-    requests.request("POST", url, data=dumps(payload), headers=headers)
+    #response = requests.get('http://' + ip + '/api/modules').json()
 
-    #return
+    firmware_path = app.config["FIRMWARE_PATH"]
+    #return dumps(firmware_path)
+
+    firmware_file_name = app.config["FIRMWARE_FILES"][module["pitch"]]
+    #return dumps(firmware_file_name)
+
+    firmware_file = firmware_path + firmware_file_name
+    #return dumps(firmware_file)
+
+    payload = {
+        "filename": firmware_file,
+        "mac": [module['mac']],
+    }
+    #return dumps(payload)
+
+    headers = {
+        'Content-Type': "multipart/form-data",
+        'Authorization': "Bearer " + auth_token
+    }
+    response = requests.request("POST", url, data=dumps(payload), headers=headers)
+
+    return response.status_code
 
 @app.route('/modules')
 def get_modules():
-    response = requests.get('http://10.0.0.3/api/modules').json()
+    ip = app.config["IP_ADDRESS"]
+    response = requests.get('http://' + ip + '/api/modules').json()
 
     moduleA = response['data'][0]['attributes']
     moduleB = response['data'][1]['attributes']
 
     modules = {}
 
-    pitchDimensions = {
-        192: 1.6,
-        160: 2.0,
-        128: 2.5,
-         80: 4.0
-    }
+    pitchDimensions = app.config["PIXEL_TO_PITCH"]
 
     if moduleA['offset']['x'] < moduleB['offset']['x']:
         modules['left'] = {
@@ -83,17 +94,6 @@ def get_modules():
 
     return modules
 
-'''
-
-
-#@app.route('/tcauth', methods=['POST'])
-#def getAuthToken():
-    #headers = {"Authorization": "Bearer 23DE481ED297204C731E398DEAEA2770AF20E96EB84111FB"}
-
-    #r = requests.post('http://10.0.0.3/api/token', data={'grant_type': 'password',})
-
-
-'''
 
 
 
