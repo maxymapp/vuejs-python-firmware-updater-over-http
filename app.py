9a5a47c09533a8ff7a17710f5ff870474971f18b
Maksym Kulikovskiy <mkulikovskiy@d3led.com>
2020-03-19 11:06:03 -0700

    moved method up the file

--- app.py
+++ app.py
@@ -30,6 +30,45 @@ def submit():
 
     return 'program finished'
 
+@app.route('/modules')
+def get_modules_to_update(left_pitch, right_pitch):
+    response = requests.get('http://' + app.config["IP_ADDRESS"] + "/api/devices").json()
+    # return dumps(response['data'])
+
+    devices = {}
+    for device in response['data']:
+        device_class = device['attributes']['device-class']
+        if device_class.startswith('TCO'):
+            mac = device['attributes']['mac']
+            devices[mac] = app.config["DEVICE_CLASS_TO_PITCH"][device_class]
+
+    # return devices
+
+    response = requests.get('http://' + app.config["IP_ADDRESS"] + "/api/modules").json()
+    # return dumps(response['data'])
+
+    if len(response['data']) < 2:
+        return "either or both modules are not found"
+    if len(response['data']) > 2:
+        return "more than 2 modules are found"
+
+    modules_to_update = []
+    for module in response['data']:
+        if(module['attributes']['offset']['x'] == 0):
+            mac = module['attributes']['mac']
+            if devices[mac] != float(left_pitch):
+                # return mac
+                modules_to_update.append({'mac': mac, 'pitch': left_pitch})
+                # update_firmware()
+        if (module['attributes']['offset']['x'] > 0):
+            mac = module['attributes']['mac']
+            if devices[mac] != float(right_pitch):
+                # update_firmware()
+                # return mac
+                modules_to_update.append({'mac': mac, 'pitch': right_pitch})
+
+    return modules_to_update
+
 @app.route('/firm')
 # def update_firmware(mac, pitch):
 def update_firmware(module):
@@ -91,45 +130,3 @@ def patch_layouts():
 def restart_scheduler():
     url = '/api/schedulers/0?command=restart'
     return "Restarting the Scheduler."
-
-@app.route('/modules')
-def get_modules_to_update(left_pitch, right_pitch):
-    response = requests.get('http://' + app.config["IP_ADDRESS"] + "/api/devices").json()
-    # return dumps(response['data'])
-
-    devices = {}
-    for device in response['data']:
-        device_class = device['attributes']['device-class']
-        if device_class.startswith('TCO'):
-            mac = device['attributes']['mac']
-            devices[mac] = app.config["DEVICE_CLASS_TO_PITCH"][device_class]
-
-    # return devices
-
-    response = requests.get('http://' + app.config["IP_ADDRESS"] + "/api/modules").json()
-    # return dumps(response['data'])
-
-    if len(response['data']) < 2:
-        return "either or both modules are not found"
-    if len(response['data']) > 2:
-        return "more than 2 modules are found"
-
-    modules_to_update = []
-    for module in response['data']:
-        if(module['attributes']['offset']['x'] == 0):
-            mac = module['attributes']['mac']
-            if devices[mac] != float(left_pitch):
-                # return mac
-                modules_to_update.append({'mac': mac, 'pitch': left_pitch})
-                # update_firmware()
-        if (module['attributes']['offset']['x'] > 0):
-            mac = module['attributes']['mac']
-            if devices[mac] != float(right_pitch):
-                # update_firmware()
-                # return mac
-                modules_to_update.append({'mac': mac, 'pitch': right_pitch})
-
-    return modules_to_update
-
-
-
